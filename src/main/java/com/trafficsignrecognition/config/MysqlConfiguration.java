289be814a1799ada1deebbaf4b6d6361c1c14502
syt123450 <421537891@qq.com>
2018-05-03 11:32:00 -0700

    add typealiases for mybatis

--- src/main/java/com/trafficsignrecognition/config/MysqlConfiguration.java
+++ src/main/java/com/trafficsignrecognition/config/MysqlConfiguration.java
@@ -66,7 +66,7 @@ public class MysqlConfiguration {
         sessionFactory.setDataSource(mysqlDataSource);
         sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver()
                 .getResources(mapperLocation));
-//        sessionFactory.setTypeAliasesPackage(typeAliasesPackage);
+        sessionFactory.setTypeAliasesPackage(typeAliasesPackage);
         return sessionFactory.getObject();
     }
 }
